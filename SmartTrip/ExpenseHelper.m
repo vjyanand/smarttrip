e49f5ab0ccdfbcaf401ce6a07671f0c75516bc4d
Vjy Anand <iavian@iavians-MacBook-Pro.local>
2014-05-03 21:14:42 -0400

    Add note

--- SmartTrip/ExpenseHelper.m
+++ SmartTrip/ExpenseHelper.m
@@ -21,6 +21,9 @@
 
 - (void)createTrip:(TripDetails *)detail completion:(ENCompletionHandler)completion {
     
+    NSString *xml = [NSString stringWithFormat:@"<?xml version=\"1.0\"?> <Itinerary xmlns=\"http://www.concursolutions.com/api/travel/trip/2010/06\"> <TripName>%@</TripName> <TravelRequestId>%@</TravelRequestId> <Bookings> <Booking> <RecordLocator>Air Locator</RecordLocator> <BookingSource>Sample Itin for Disrupt</BookingSource> <DateBookedLocal>2014-04-30T03:47:14</DateBookedLocal></Booking> </Bookings> </Itinerary>", detail.destination, detail.UUID];
+    
+    
 }
 
 - (void)addNote:(Expense *)expense forTrip:(NSString *)tripUUID completion:(ENCompletionHandler)completion {
